20c7d7ef96104c29fe6c3f314a89d35c7ae3e80e
Daniel <esc.daniel91@gmail.com>
2024-01-15 17:15:38 -0500

    map phase in progress

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,38 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	files   []string
+	nReduce int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+// rpc for workers to request a task
+func (c *Coordinator) RequestTask(args *RequestTaskArgs, reply *RequestTaskReply) error {
+	fmt.Printf("worker requested a task\n")
+	reply.FileToProcess = c.files[0]
+	return nil
+}
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +46,25 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
+	c.files = files
+	c.nReduce = nReduce
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,74 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func writeJSON(file string) {
+	//enc := json.NewEncoder(file)
+	//for _, kv := ... {
+	//  err := enc.Encode(&kv)
+}
+func readJSON(file string) {
+	//dec := json.NewDecoder(file)
+	//for {
+	//  var kv KeyValue
+	//  if err := dec.Decode(&kv); err != nil {
+	//	break
+	//  }
+	//  kva = append(kva, kv)
+	//}
+}
 
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
+	//CallExample()
+	filename, err := CallRequestTask()
+	if err != nil {
+		log.Fatalf("Rpc error in CallRequestTask %v", filename)
+	}
+	fmt.Printf("fileToProcess %v\n", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error: opening file %v", file)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Error getting file content for %v", file)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	fmt.Printf("kva %v\n", kva)
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +93,25 @@ func CallExample() {
 	}
 }
 
-//
+// request task RPC
+func CallRequestTask() (string, error) {
+	args := RequestTaskArgs{}
+	reply := RequestTaskReply{}
+
+	ok := call("Coordinator.RequestTask", &args, &reply)
+	if ok {
+		fmt.Printf("task requested\n")
+		return reply.FileToProcess, nil
+	} else {
+		errorMsg := "request task call failed!\n"
+		fmt.Printf(errorMsg)
+		return "", errors.New(errorMsg)
+	}
+}
+
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
